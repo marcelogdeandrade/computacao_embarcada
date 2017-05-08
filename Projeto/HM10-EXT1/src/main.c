0ddb23ad4603497f88d07b91681d43eae1d8d4ac
Marcelo Andrade <Marcelo Andrade>
2017-05-08 17:58:32 -0300

    partial code to aula 14 (same project as aula 13).

--- Projeto/HM10-EXT1/src/main.c
+++ Projeto/HM10-EXT1/src/main.c
@@ -1,64 +1,31 @@
 #include "asf.h"
 
-/************************************************************************/
-/* DEFINES                                                              */
-/************************************************************************/
-/**
- *  Informacoes para o RTC
- *  poderia ser extraida do __DATE__ e __TIME__
- *  ou ser atualizado pelo PC.
- */
-#define YEAR        2017
-#define MOUNTH      3
-#define DAY         27
-#define WEEK        13
-#define HOUR        9
-#define MINUTE      5
-#define SECOND      0
-
 /**
  * LEDs
  */
-#define LED_PIO_ID		ID_PIOC
+#define LED_PIO_ID		  ID_PIOC
 #define LED_PIO         PIOC
-#define LED_PIN		    8
+#define LED_PIN		      8
 #define LED_PIN_MASK    (1<<LED_PIN)
 
 /**
  * Botão
  */
-#define BUT_PIO_ID      ID_PIOA
-#define BUT_PIO         PIOA
-#define BUT_PIN		    11
-#define BUT_PIN_MASK    (1 << BUT_PIN)
+#define BUT_PIO_ID            ID_PIOA
+#define BUT_PIO               PIOA
+#define BUT_PIN		            11
+#define BUT_PIN_MASK          (1 << BUT_PIN)
 #define BUT_DEBOUNCING_VALUE  79
 
-
-/** XDMAC channel used in this example. */
-#define XDMAC_TX_CH 0
-#define XDMAC_RX_CH 1
-
-/** XDMAC channel HW Interface number for SPI0,refer to datasheet. */
-#define USART_XDMAC_TX_CH_NUM  XDAMC_CHANNEL_HWID_USART1_TX
-#define USART_XDMAC_RX_CH_NUM  XDAMC_CHANNEL_HWID_USART1_RX
-
-/** XDMAC channel configuration. */
-static xdmac_channel_config_t xdmac_tx_cfg,xdmac_rx_cfg;
-
-/** The buffer size for transfer  */
-#define BUFFER_SIZE          100
+/** 
+ *  USART
+ */
 
 /************************************************************************/
 /* VAR globais                                                          */
 /************************************************************************/
-volatile uint8_t flag_led0 = 1;
 
-/** 
- * XDMA
- */
-uint8_t tx_buffer[]             = "This is message from SPI master transferred by XDMAC test";
-uint8_t rx_buffer[BUFFER_SIZE]  = "0";
-uint32_t g_size                 = sizeof(tx_buffer);
+volatile uint8_t flag_led0 = 1;
 
 /************************************************************************/
 /* PROTOTYPES                                                           */
@@ -66,11 +33,7 @@ uint32_t g_size                 = sizeof(tx_buffer);
 
 void BUT_init(void);
 void LED_init(int estado);
-void TC1_init(void);
-void RTC_init(void);
 void pin_toggle(Pio *pio, uint32_t mask);
-static void uart_xdmac_configure(Usart *const pusart);
-
 
 /************************************************************************/
 /* Handlers                                                             */
@@ -129,87 +92,129 @@ void LED_init(int estado){
 
 /**
  * \brief Configure UART console.
+ * BaudRate : 115200
+ * 8 bits
+ * 1 stop bit
+ * sem paridade
  */
+
 static void configure_console(void)
 {
+
+  /* Configura USART1 Pinos */
+  sysclk_enable_peripheral_clock(ID_PIOB);
+  sysclk_enable_peripheral_clock(ID_PIOA);
+  pio_set_peripheral(PIOB, PIO_PERIPH_D, PIO_PB4);  // RX
+  pio_set_peripheral(PIOA, PIO_PERIPH_A, PIO_PA21); // TX
+ 	MATRIX->CCFG_SYSIO |= CCFG_SYSIO_SYSIO4;
+ 
 	const usart_serial_options_t uart_serial_options = {
-		.baudrate =		CONF_UART_BAUDRATE,
-		.charlength =	CONF_UART_CHAR_LENGTH,
-		.paritytype =	CONF_UART_PARITY,
-		.stopbits =		CONF_UART_STOP_BITS,
+		.baudrate   = CONF_UART_BAUDRATE,
+		.charlength = CONF_UART_CHAR_LENGTH,
+		.paritytype = CONF_UART_PARITY,
+		.stopbits   = CONF_UART_STOP_BITS,
 	};
 
-	/* Configure UART console. */
+	/* Configure console UART. */
 	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
 	stdio_serial_init(CONF_UART, &uart_serial_options);
 }
 
+/**
+ * \brief Configure UART console.
+ */
+static void USART0_init(void){
+  
+  /* Configura USART0 Pinos */
+  sysclk_enable_peripheral_clock(ID_PIOB);
+  pio_set_peripheral(PIOB, PIO_PERIPH_C, PIO_PB0);
+  pio_set_peripheral(PIOB, PIO_PERIPH_C, PIO_PB1);
+  
+  /* Configura opcoes USART */
+  const sam_usart_opt_t usart_settings = {
+    .baudrate     = 9600,
+    .char_length  = US_MR_CHRL_8_BIT,
+    .parity_type  = US_MR_PAR_NO,
+    .stop_bits    = US_MR_NBSTOP_1_BIT,
+    .channel_mode = US_MR_CHMODE_NORMAL
+  };
+
+  /* Ativa Clock periferico USART0 */
+  sysclk_enable_peripheral_clock(ID_USART0);
+  
+  /* Configura USART para operar em modo RS232 */
+  usart_init_rs232(USART0, &usart_settings, sysclk_get_peripheral_hz());
+  
+  /* Enable the receiver and transmitter. */
+	usart_enable_tx(USART0);
+	usart_enable_rx(USART0);
+ }
+
+/**
+ *  Envia para o UART uma string
+ */
+uint32_t usart_putString(uint8_t *pstring){
+  uint32_t i = 0 ;
+
+  while(*(pstring + i)){
+    usart_serial_putchar(USART0, *(pstring+i++));
+    while(!uart_is_tx_empty(USART0)){};
+  }    
+     
+  return(i);
+}
+
+/*
+ * Busca do UART uma string
+ */
+uint32_t usart_getString(uint8_t *pstring){
+  uint32_t i = 0 ;
+  
+  usart_serial_getchar(USART0, (pstring+i));
+  while(*(pstring+i) != '\n'){
+    usart_serial_getchar(USART0, (pstring+(++i)));
+  }
+  *(pstring+i+1)= 0x00;
+  return(i);
+  
+}
 
 /************************************************************************/
 /* Main Code	                                                        */
 /************************************************************************/
 int main(void){
   
-      char buf[100];
+  /* buffer para recebimento de dados */
+  uint8_t bufferRX[100];
+  uint8_t bufferTX[100];
 
-  
-	  /* Initialize the SAM system */
-	  sysclk_init();
-    
-    /* Disable the watchdog */
-	  WDT->WDT_MR = WDT_MR_WDDIS;
-    
-    board_init();
-
-    /* Configura Leds */
-    LED_init(1);
-	
-	  /* Configura os botões */
-	  BUT_init();  
-  
-    
-    /* delay */
-    uint32_t cpuFreq = sysclk_get_cpu_hz();
-    delay_init(cpuFreq);
+  /* Initialize the SAM system */
+  sysclk_init();
    
-    /* CONFIGURE USART0 */
-    sysclk_enable_peripheral_clock(ID_PIOB);
-    pio_set_peripheral(PIOB, PIO_PERIPH_C, PIO_PB0);
-    pio_set_peripheral(PIOB, PIO_PERIPH_C, PIO_PB1);
-          
-    const usart_serial_options_t uart_serial_options = {
-      	.baudrate =		9600,
-      	.charlength =	US_MR_CHRL_8_BIT,
-      	.paritytype =	US_MR_PAR_NO,
-      	.stopbits =		US_MR_NBSTOP_1_BIT,
-    	};
-      
-   	sysclk_enable_peripheral_clock(ID_USART0);
-    stdio_serial_init(USART0, &uart_serial_options);
-    
-   // pmc_enable_periph_clk(ID_PIOD);
-   // pio_set_output(PIOD, (1<<28), 0, 0, 0 );    
-   // pio_clear(PIOD, (1<<28));
-   
-   	//sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
-   	//stdio_serial_init(CONF_UART, &uart_serial_options);
-
-   // usart_putchar(USART0, 0xFA);
-    
-   // while(usart_read(USART0, &buf[0]);)
-    //usart_read(USART0, &buf[0]);
-
-  char bufferRX[100];
-
+  /* Disable the watchdog */
+  WDT->WDT_MR = WDT_MR_WDDIS;
+  
+  /* Inicializa com serial com PC*/
+  configure_console();
+  
+  /* Configura Leds */
+  LED_init(1);
+  
+  /* Configura os botões */
+  BUT_init();  
+  
+  /* Configura USART0 para comunicacao com o HM-10 */
+  USART0_init();
+ 
+  /* Inicializa funcao de delay */
+  delay_init( sysclk_get_cpu_hz());
+        
 	while (1) {
-		/* Entra em modo sleep */
-		 //printf("AT+ROLE=1\r\n");
-    printf("%s \n", "Comp-Embarcada" );
-    gets(bufferRX);
-    //usart_putchar(USART0, 0xFA);
-    //printf("%AT\r\n");
-    delay_ms(500);
-
-
+		sprintf(bufferTX, "%s \n", "Ola Voce");
+		printf("Olá Você \n");
+		usart_putString(bufferTX);
+		//pin_toggle(LED_PIO, LED_PIN_MASK);
+	 //   usart_getString(bufferRX);
+	//		delay_s(1);
 	}
 }
